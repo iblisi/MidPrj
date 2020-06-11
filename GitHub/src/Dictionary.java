7b0c8b32d53ea7727d5dabcc27374c546f43b38e
inisu <inisu@inisu>
2020-06-11 18:28:24 +0900

    1-8.4

--- GitHub/src/Dictionary.java
+++ GitHub/src/Dictionary.java
@@ -1,17 +1,25 @@
 import java.awt.Choice;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 import javax.swing.table.DefaultTableModel;
-
-import java.awt.Color;
-import java.awt.Font;
 
 public class Dictionary {
 	JFrame f;
@@ -19,7 +27,7 @@ public class Dictionary {
 	private JTextField search, list, hanja;
 	JButton button;
 	Choice sel;
-	
+
 	private JTable table;
 	JLabel listname;
 	DefaultTableModel model;
@@ -29,8 +37,9 @@ public class Dictionary {
 	Object ob[][] = new Object[0][5];
 
 	DAO dao;
-	
-	
+
+	Image img = new ImageIcon("C:\\Users\\inisu\\Desktop\\배경2.jpg").getImage();
+	private JLabel bgi;
 
 	public Dictionary() {
 
@@ -41,7 +50,7 @@ public class Dictionary {
 		}
 
 		f = new JFrame("dictionary");
-		f.getContentPane().setBackground(new Color(0, 191, 255));
+		f.getContentPane().setBackground(Color.white);
 		f.setBounds(30, 50, 600, 500);
 		f.getContentPane().setLayout(null);
 
@@ -54,6 +63,7 @@ public class Dictionary {
 		search.setBounds(100, 71, 364, 23);
 		f.getContentPane().add(search);
 		search.setColumns(10);
+		search.setOpaque(false);
 		search.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				search.setText("");
@@ -61,55 +71,71 @@ public class Dictionary {
 		});
 
 		list = new JTextField();
-		list.setBounds(308, 390, 232, 21);
+		list.setBounds(338, 390, 200, 21);
 		f.getContentPane().add(list);
 		list.setColumns(10);
 		list.setEditable(false);
+		list.setOpaque(false);
 
 		listname = new JLabel("검색 한 단어");
-		listname.setBounds(227, 390, 76, 23);
+		listname.setBounds(260, 390, 76, 23);
 		f.getContentPane().add(listname);
 
-		kor = new JCheckBox("한국어",true);
-		kor.setBounds(403, 31, 61, 23);
+		kor = new JCheckBox(" 한국어", true);
+		kor.setBounds(403, 31, 65, 23);
 		f.getContentPane().add(kor);
 		kor.setOpaque(false);
 
-		jp = new JCheckBox("일본어");
+		jp = new JCheckBox(" 일본어");
 		jp.setBounds(476, 31, 64, 23);
 		f.getContentPane().add(jp);
 		jp.setOpaque(false);
 
 		sel = new Choice();
 		sel.setBounds(30, 71, 64, 13);
-		
+
 		sel.add(" 선  택 ");
 		sel.add(" 음  독 ");
 		sel.add(" 훈  독 ");
 		f.getContentPane().add(sel);
-   
+
 //		model = new DefaultTableModel(ob, str);
 		model = dao.model;
 		table = new JTable(model);
 		jsp = new JScrollPane(table);
 		jsp.setBounds(30, 130, 350, 200);
 		f.getContentPane().add(jsp);
-		
-		
-		
+
 		table.getColumnModel().getColumn(4).setMinWidth(0);
 		table.getColumnModel().getColumn(4).setMaxWidth(0);
 		table.getColumn("음 독").setPreferredWidth(40);
 		table.getColumn(" 음 독").setPreferredWidth(40);
-	
-		hanja = new JTextField();
+
+		hanja = new JTextField() {
+			public void setBorder(Border border) {
+			}
+		};
+		hanja.setOpaque(false);
 		hanja.setBounds(395, 130, 150, 200);
 		f.getContentPane().add(hanja);
 		hanja.setColumns(10);
 		hanja.setEditable(false);
-		
+
+		bgi = new JLabel() {
+			{
+				setOpaque(false);
+			}
+
+			public void paintComponent(Graphics g) {
+				g.drawImage(img, 0, 0, null);
+				super.paintComponent(g);
+			}
+		};
+		bgi.setBounds(0, 0, 584, 461);
+		f.getContentPane().add(bgi);
+
 		f.setVisible(true);
-		
+
 		f.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
@@ -119,7 +145,7 @@ public class Dictionary {
 		table.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				int sel = table.getSelectedRow();
-			    String result_han = (String) table.getValueAt(sel, 4);	
+				String result_han = (String) table.getValueAt(sel, 4);
 				hanja.setText(result_han);
 				hanja.setFont(new Font("", Font.PLAIN, 150));
 			}
