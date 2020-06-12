ef3b534797ed3990771985c8f2a10c0f3b5ab2d3
inisu <inisu@inisu>
2020-06-12 17:42:04 +0900

    9.1 엔터시 검색되게함

--- GitHub/src/Dictionary.java
+++ GitHub/src/Dictionary.java
@@ -5,11 +5,15 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -20,6 +24,9 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 import javax.swing.table.DefaultTableModel;
+
+// 검색결과 창에 나온 단어를 눌렀을 때 다시 그 검색화면이 나올 수 있도록
+// sql로더를 이용해서 데이터 베이스에 한자가 깨지지 않고 입력가능한지 확인
 
 public class Dictionary {
 	JFrame f;
@@ -153,46 +160,15 @@ public class Dictionary {
 
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				select();
 
-				if (search.getText() == null) {
-					list.setText(" ");
-				} else {
-					for (int i = 0; i < slist.length; i++) {
-						slist[i] += search.getText() + ", ";
-						list.setText(" " + slist[i]);
-					}
-				}
+			}
 
-				if (kor.isSelected()) {
-					if (sel.getSelectedIndex() == 1) {
-						model.setNumRows(0);
-						dao.selectKs(search.getText());
-						model = dao.getModel();
-						getHanja();
-					} else if (sel.getSelectedIndex() == 2) {
-						model.setNumRows(0);
-						dao.selectKm(search.getText());
-						model = dao.getModel();
-						getHanja();
-					} else {
-						search.setText("선택 사항을 선택 해주세요.");
-					}
-				} else if (jp.isSelected()) {
-					if (sel.getSelectedIndex() == 1) {
-						model.setNumRows(0);
-						dao.selectJs(search.getText());
-						model = dao.getModel();
-						getHanja();
-					} else if (sel.getSelectedIndex() == 2) {
-						model.setNumRows(0);
-						dao.selectJm(search.getText());
-						model = dao.getModel();
-						getHanja();
-					} else {
-						search.setText("선택 사항을 선택 해주세요.");
-					}
-				} else {
-					search.setText("한국어나 일본어를 체크해 주세요.");
+		});
+		search.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+					select();
 				}
 			}
 		});
@@ -203,6 +179,49 @@ public class Dictionary {
 		table.getColumn("한자").setWidth(0);
 		table.getColumn("한자").setMinWidth(0);
 		table.getColumn("한자").setMaxWidth(0);
+	}
+
+	public void select() {
+		if (search.getText() == null) {
+			list.setText(" ");
+		} else {
+			for (int i = 0; i < slist.length; i++) {
+				slist[i] += search.getText() + ", ";
+				list.setText(" " + slist[i]);
+			}
+		}
+
+		if (kor.isSelected()) {
+			if (sel.getSelectedIndex() == 1) {
+				model.setNumRows(0);
+				dao.selectKs(search.getText());
+				model = dao.getModel();
+				getHanja();
+			} else if (sel.getSelectedIndex() == 2) {
+				model.setNumRows(0);
+				dao.selectKm(search.getText());
+				model = dao.getModel();
+				getHanja();
+			} else {
+				search.setText("선택 사항을 선택 해주세요.");
+			}
+		} else if (jp.isSelected()) {
+			if (sel.getSelectedIndex() == 1) {
+				model.setNumRows(0);
+				dao.selectJs(search.getText());
+				model = dao.getModel();
+				getHanja();
+			} else if (sel.getSelectedIndex() == 2) {
+				model.setNumRows(0);
+				dao.selectJm(search.getText());
+				model = dao.getModel();
+				getHanja();
+			} else {
+				search.setText("선택 사항을 선택 해주세요.");
+			}
+		} else {
+			search.setText("한국어나 일본어를 체크해 주세요.");
+		}
 	}
 
 	public static void main(String[] args) {
